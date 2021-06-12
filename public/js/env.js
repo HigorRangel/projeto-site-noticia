6e4a8b9244c6f4cee072ee9cf70ed5ab3df96f90
HigorRangel <higorrg2000@hotmail.com>
2021-06-12 17:29:12 -0300

    Commit de início da entrega 3 do projeto

---
+++ public/js/env.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+  apiKey: 'AIzaSyAkCbxVqaWE-1Hwc8xGj5oKdTKgFugFlo4',
+  authDomain: 'projeto-site-noticia.firebaseapp.com',
+  projectId: 'projeto-site-noticia',
+  storageBucket: 'projeto-site-noticia.appspot.com',
+  messagingSenderId: '531531812993',
+  appId: '1:531531812993:web:f45c3b8e4b7b050f2a7d80',
+  measurementId: 'G-GEBH1XH3L6',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
